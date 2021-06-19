e2e17bc68e370f377904d7d213ff57cf336650ef
hosseinkasiri <hosseinkasiri5@gmail.com>
2021-06-19 17:31:39 +0430

    create product model

---
+++ app/src/main/java/com/example/noavar/model/Product.kt
@@ -0,0 +1,15 @@
+package com.example.noavar.model
+
+import com.squareup.moshi.Json
+
+data class Product(
+    @Json(name = "product_id")
+    val id: Int,
+    @Json(name = "product_title")
+    val title: String,
+    @Json(name = "product_picture")
+    val picture: String,
+    @Json(name = "product_regular_price")
+    val regularPrice: String,
+    @Json(name = "product_sale_price")
+    val salePrice: String)
